80b04e66d265df50adc2279fc39b73517fb6a50c
Nagao-hikaru <koyanagi29sushi@gmail.com>
2021-05-06 19:43:11 +0900

    v-if v-show

--- src/laravelapp/public/vuejs/hello/js/main.js
+++ src/laravelapp/public/vuejs/hello/js/main.js
@@ -3,15 +3,6 @@ var app = new Vue({
   data: {
     message: 'Hello Vue.js',
     count: 0,
-    user: {
-      lastname: 'yamada',
-      firstname: 'syouta',
-      prefecture: 'Tokyo',
-    },
-    colors: [
-      'Red',
-      'Green',
-      'Blue',
-    ]
+    toggle: true,
   }
 })
