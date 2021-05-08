eb8ffd0e5ae9c8a206c984f923ecc285cd0c3df3
Nagao-hikaru <koyanagi29sushi@gmail.com>
2021-05-08 11:15:47 +0900

    DOMが更新されない

--- src/laravelapp/public/vuejs/hello/js/main.js
+++ src/laravelapp/public/vuejs/hello/js/main.js
@@ -1,14 +1,28 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js',
-    count: 0,
-    toggle: true,
-    colors: ['Red', 'Green', 'Blue'],
-    user: {
-      firstname: 'tarou',
-      lastname: 'yamada',
-      age: 20,
+    counter: 0,
+  	now: 'vdd',
+  },
+  methods: {
+  	onclick: function() {
+    	// alert('onclick!');
+      this.now = new Date().toLocaleString();
     }
   }
 })
+
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Hello Vue.js',
+//     count: 0,
+//     toggle: true,
+//     colors: ['Red', 'Green', 'Blue'],
+//     user: {
+//       firstname: 'tarou',
+//       lastname: 'yamada',
+//       age: 20,
+//     }
+//   }
+// });
